efd8c171865f1d0433d95b748153738d61f86fb8
Jason Pena <jasonpena@awkless.com>
2024-06-23 13:30:31 -0600

    feat: Define `main`
    Currently, `main` provides a basic entry point into the binary. Nothing has
    been implemented yet.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/rally.rs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+/// Rally binary entry point.
+///
+/// # Postconditions
+///
+/// 1. Currently just prints sample output.
+fn main() {
+    println!("Hello from Rally!");
+}
